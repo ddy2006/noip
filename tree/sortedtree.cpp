d9bc7380fbf6c8bca3460b21585198a71c5833fb
Mengyang Wu <ddy2006@yeah.net>
2019-03-03 19:56:48 +0800

    update sorted tree

--- tree/sortedtree.cpp
+++ tree/sortedtree.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+//定义节点
 struct Node{
     char val;
     struct Node *left, *right;
 };
-
+//构造二叉树
 void add(Node* &p, char val){
     if(p==NULL){
         Node *ptr = new Node;
@@ -17,6 +18,7 @@ void add(Node* &p, char val){
         add(p->right,val);
     }
 }
+//中序遍历输出
 void print(Node *p){
     if(p!=NULL){
         print(p->left);
@@ -27,11 +29,11 @@ void print(Node *p){
 
 int main(){
     char line[300];
-    gets(line);
+    gets(line);     //整行输入
     struct Node *root = NULL;
     for(int i=0;i<strlen(line);i++){
         char ch=line[i];
-        if(!isalnum(ch)){
+        if(!isalnum(ch)){   //错误即输出“ERROR”
             cout<<"ERROR"<<endl;
             return 1;
         }
